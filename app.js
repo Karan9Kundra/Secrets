81ca059784008becb0d0166c30dc44c5c7c484d3
k.kundra2012@gmail.com <Gitkaran!234>
2022-03-15 18:33:54 -0400

    heroku

--- app.js
+++ app.js
@@ -144,8 +144,8 @@ app.post("/login", function(req, res){
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server started on port 3000.");
+  console.log(`Server started on port ${port}.`);
 });
